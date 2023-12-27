a239a008ec3e624719a350401f71d1e2ee718a8e
jonany <jonany>
2023-12-27 02:23:33 -0500

    copied replace patterns from detox cmd

--- src/detox.ts
+++ src/detox.ts
@@ -1,11 +1,12 @@
-import filenamify from "filenamify";
-import slugify from "slugify";
-
-// TODO: duplicate detox behavior
-export const detox = (val: string) => slugify(
-    filenamify(
-        slugify(val, { replacement: '_', locale: 'en', trim: true, }),
-        { replacement: '_', }
-    ),
-    { replacement: '_', locale: 'en', trim: true, }
-);
+export const detox = (val: string): string => {
+  return val
+    .replace(
+      /[\u0001-\u0009\u000a-\u000f\u0010-\u0019\u001a-\u001f\u007f\u0020-\u0022\u0024\u0027\u002a\u002f\u003a-\u003c\u003e\u003f\u0040\u005c\u0060\u007c]/g,
+      '_'
+    )
+    .replace(
+      /[\u0028\u0029\u005b\u005d\u007b\u007d]/g,
+      '-'
+    )
+    .replace(/(\-|_){2,}/g, '$1');
+};
